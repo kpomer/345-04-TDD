b0c9f29d785e5034555373651eae7c11a82cd434
Kevin Pomer <kpomer@ithaca.edu>
2018-01-30 21:14:21 -0500

    Tests written for case with two values in string

--- test/edu/ithaca/pomer/stringCalculatorTest.java
+++ test/edu/ithaca/pomer/stringCalculatorTest.java
@@ -8,11 +8,20 @@ class stringCalculatorTest {
 
     @org.junit.jupiter.api.Test
     public void Add(){
+        //Empty String
         assertEquals(0, myCalc.Add(""), "should return 0 for an empty string");
 
+        //One value in string
         assertEquals(0, myCalc.Add("0"), "should return 0");
         assertEquals(1, myCalc.Add("1"), "should return 1");
         assertEquals(-3, myCalc.Add("-3"), "should return -3");
+
+        //Two values in string
+        assertEquals(0, myCalc.Add("0,1"), "should return 1 for an empty string");
+        assertEquals(0, myCalc.Add("5,5"), "should return 10 for an empty string");
+        assertEquals(0, myCalc.Add("3,-4"), "should return -1 for an empty string");
+        assertEquals(0, myCalc.Add("0,0"), "should return 0 for an empty string");
+        assertEquals(0, myCalc.Add("-3,-4"), "should return -7 for an empty string");
     }
 
 }
